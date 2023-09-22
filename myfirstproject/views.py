a603b52504c18117f2d2d8680ff3370c5a7b26b6
nikitosik <nikitadanko2002@gmail.com>
2023-09-23 00:31:16 +0300

    all

--- myfirstproject/views.py
+++ myfirstproject/views.py
@@ -5,4 +5,5 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request,'reverse.html',{'usertext':user_text,'reversed_text':reversed_text})
+    count_words = len(user_text.split())
+    return render(request,'reverse.html',{'usertext':user_text,'reversed_text':reversed_text,'count_words':count_words})
